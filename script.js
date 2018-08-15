bd88460516e9839970d215cdf3f44a43173aa8dc
Natasa <natasacb@gmail.com>
2018-08-15 12:54:17 +0200

    changed keywords

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function findColor() {
-  var str = "0123456789ABCDEF";
-  var arr = str.split("");
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
+  const str = "0123456789ABCDEF";
+  let arr = str.split("");
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
       color += arr[Math.floor(Math.random() * 16)];
   }
   return color;
 }
 
 function showColor() {
-  var myColor = findColor();
+  const myColor = findColor();
   $("#colorbox").css("background-color", myColor);
   $("#colorname").css("color", myColor);
   document.getElementById("colorname").innerHTML = myColor;
